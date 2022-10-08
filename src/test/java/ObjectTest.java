d28b88f2e6ab294712afdbeeeb94fd02c2e8ac7b
MECCNR <e.migalckin@yandex.ru>
2022-10-08 19:16:54 +0900

    "v1"

--- src/test/java/ObjectTest.java
+++ src/test/java/ObjectTest.java
@@ -10,36 +10,30 @@ public class ObjectTest {
         data.enterAccount();
     }
 
-    @Test
-    @Order(1)
-    void checkMoneyofFirstAccount() {
-        int actual = data.getCardBalance(data.firstCard);
-        int expected = 10000;
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    @Order(2)
-    void checkMoneyofSecondAccount() {
-        int actual = data.getCardBalance(data.secondCard);
-        int expected = 10000;
-        Assertions.assertEquals(expected, actual);
-    }
-
     // Этот тест очень сложно устроен! Поэтому объясняю---
     // У вас есть две карты. Первая (data.firstCard), и вторая (data.secondCard)
     // У этих карт есть номера (!ВАЖНО). Первый номер для первой карты (data.firstNumber)
     // и второй для второй (data.secondNumber)
 
     @Test
-    @Order(3)
-    void TransferTest() {
+    void TransferTestFirstCard() {
         int originalMoney = data.getCardBalance(data.secondCard); // начальное количество денег. В скобках добавьте карту для которой вы хотите пополнить счёт
         int howMuchMoneyDoYouWantToAdd = 150; // здесь напишите сколько вы хотите добавить денег
         int expected = originalMoney + howMuchMoneyDoYouWantToAdd;
         data.transferMoney(data.secondCard, data.firstNumber, howMuchMoneyDoYouWantToAdd); // ОЧЕНЬ ВАЖНО! В скобах впишите сначала данные карты которой вы хотите
         // пополнить счёт. После запятой запишите данные НОМЕРА с карты которой вы хотите забрать деньги. В третьей ничего не меняйте.
         int actual = data.getCardBalance(data.secondCard); // в скобках этого метода должна быть карта в которой был пополнен счёт
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void TransferTestSecondCard() {
+        int originalMoney = data.getCardBalance(data.firstCard); // начальное количество денег. В скобках добавьте карту для которой вы хотите пополнить счёт
+        int howMuchMoneyDoYouWantToAdd = 150; // здесь напишите сколько вы хотите добавить денег
+        int expected = originalMoney + howMuchMoneyDoYouWantToAdd;
+        data.transferMoney(data.firstCard, data.secondNumber, howMuchMoneyDoYouWantToAdd); // ОЧЕНЬ ВАЖНО! В скобах впишите сначала данные карты которой вы хотите
+        // пополнить счёт. После запятой запишите данные НОМЕРА с карты которой вы хотите забрать деньги. В третьей ничего не меняйте.
+        int actual = data.getCardBalance(data.firstCard); // в скобках этого метода должна быть карта в которой был пополнен счёт
         Assertions.assertEquals(expected, actual);
     }
 }
